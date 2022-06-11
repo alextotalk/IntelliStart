f7ce3d2f17e0b6239de326a10720d104c4d5cf38
alextotalk <alexoo@inbox.ru>
2022-06-11 16:19:03 +0300

    Testing slice

---
+++ Slice.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func sliceTest() {
+	//slice := make([]int, 3, 3)
+	slice := make([]int, 3, 3)
+	slice[0] = 1
+	slice[1] = 2
+	slice[2] = 3
+	fmt.Println(slice)
+	handle(slice)
+	fmt.Println(slice)
+	//var slice = []int{1, 2, 3, 4, 5}
+	//fmt.Println(double(slice))
+
+	//var list []int
+	//print(list == nil, " - ;")
+	//print(len(list) == 0, " - ;")
+	//fmt.Println(append(list, 777))
+	////
+	////var list2 = []int{}
+	////print(list2 == nil, " - ;")
+	////print(len(list2) == 0, " - ;")
+	//var slice = make([]int, 5, 10)
+	//fmt.Println("Length=", len(slice), " - Capacity=", cap(slice))
+
+}
+
+func handle(slice []int) {
+	//slice[1] = 77
+	//list := append(slice, 7777)
+	//fmt.Println(list)
+	//	It`s wrong we copy link so meaning parent slice changed.
+	//	We need to crate new slice and copy meanings to it!
+	newSlice := make([]int, len(slice))
+	copy(newSlice, slice)
+	newSlice = append(newSlice, 777)
+	fmt.Println(newSlice)
+}
+
+//func double(slice []int) []int {
+//	//res:=[]int{} //wrong we recreate after write every time
+//	//res := make([]int, 0, len(slice))
+//	//for _, num := range slice {
+//	//	res = append(res, num*2)
+//	//}
+//	//or
+//	res := make([]int, len(slice))
+//	for i, num := range slice {
+//		res[i] = num * 2
+//	}
+//	return res
+//}
--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+//func one(n int) {
+//	fmt.Println(n)
+//	if n > 1 {
+//		i := n - 1
+//		one(i)
+//	}
+//
+//}
+
 func main() {
-	fmt.Println("Hello world! Bro!")
+	fmt.Println("Hello world! Bro1!")
+	slice := make([]int, 3, 3)
+	slice[0] = 1
+	slice[1] = 2
+	slice[2] = 3
+	fmt.Println(slice)
+	handle(slice)
+	fmt.Println(slice)
+	sliceTest()
 }
