a0ec0770ea3931de7b8a716964acd222b1a0d50b
alextotalk <alexoo@inbox.ru>
2022-06-20 14:16:20 +0300

    HW2

--- hw2/1main.go
+++ hw2/1main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // Одне яблуко коштує 5.99 грн. Ціна однієї груші - 7 грн.
@@ -20,46 +21,45 @@ import (
 // Створити папку в своєму репозиторії в github та завантажити туди 1main.go файл, в котрому буде зроблено дане завдання.
 func main() {
 	const (
-		priceApple = 599
-		pricePear  = 700
-		ourWallet  = 2300
+		priceApple = 5.99
+		pricePear  = 7.00
+		ourWallet  = 23.00
 	)
 	print("----------------------/1/----------------------\n\n")
 
 	print("\n1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?\n")
-	amountApple := 900
-	amountPear := 800
+	amountApple := 9.00
+	amountPear := 8.00
 	result := amountApple*priceApple + amountPear*pricePear
 	resultInString := fmt.Sprintf("%.2f", result)
-
 	println("-->Відповідь: Щоб купити 9 яблук і 8 груш нам потрібно", resultInString, " грн. \n")
-	//
-	//	print("----------------------/2/----------------------\n\n")
-	//
-	//	print(" 2. Скільки груш ми можемо купити?\n")
-	//	howMachPearsCanWeBuy := ourWallet / pricePear
-	//	resultInString = fmt.Sprintf("%.f", math.Floor(howMachPearsCanWeBuy))
-	//	println("-->Відповідь: Ми можемо купити", resultInString, " груші!\n")
-	//
-	//	print("----------------------/3/----------------------\n\n")
-	//
-	//	print(" 3. Скільки яблук ми можемо купити?\n")
-	//	howMachAppleCanWeBuy := ourWallet / priceApple
-	//	resultInString = fmt.Sprintf("%.f", math.Floor(howMachAppleCanWeBuy))
-	//	println("-->Відповідь: Ми можемо купити", resultInString, " яблук!\n")
-	//
-	//	print("----------------------/4/----------------------\n\n")
-	//
-	//	print(" 4. Чи ми можемо купити 2 груші та 2 яблука?\n")
-	//	priceTwoApplesAndTowPears := 2*priceApple + 2*pricePear
-	//	if ourWallet <= priceTwoApplesAndTowPears {
-	//		resultInString = "не"
-	//	} else {
-	//		resultInString = ""
-	//	}
-	//	println("-->Відповідь: Ми", resultInString, "можемо купити 2 груші та 2 яблука !\n")
-	//	fmt.Printf("-->Відповідь: Ми %v можемо купити 2 груші та 2 яблука !\n", resultInString)
-	//
-	//	print("----------------------//----------------------\n\n")
-	//
+
+	print("----------------------/2/----------------------\n\n")
+
+	print(" 2. Скільки груш ми можемо купити?\n")
+	howMachPearsCanWeBuy := ourWallet / pricePear
+	resultInString = fmt.Sprintf("%.f", math.Floor(howMachPearsCanWeBuy))
+	println("-->Відповідь: Ми можемо купити", resultInString, " груші!\n")
+
+	print("----------------------/3/----------------------\n\n")
+
+	print(" 3. Скільки яблук ми можемо купити?\n")
+	howMachAppleCanWeBuy := ourWallet / priceApple
+	resultInString = fmt.Sprintf("%.f", math.Floor(howMachAppleCanWeBuy))
+	println("-->Відповідь: Ми можемо купити", resultInString, " яблук!\n")
+
+	print("----------------------/4/----------------------\n\n")
+
+	print(" 4. Чи ми можемо купити 2 груші та 2 яблука?\n")
+	priceTwoApplesAndTowPears := 2*priceApple + 2*pricePear
+	if ourWallet <= priceTwoApplesAndTowPears {
+		resultInString = "не"
+	} else {
+		resultInString = ""
+	}
+	println("-->Відповідь: Ми", resultInString, "можемо купити 2 груші та 2 яблука !\n")
+	fmt.Printf("-->Відповідь: Ми %v можемо купити 2 груші та 2 яблука !\n", resultInString)
+
+	print("----------------------//----------------------\n\n")
+
 }
